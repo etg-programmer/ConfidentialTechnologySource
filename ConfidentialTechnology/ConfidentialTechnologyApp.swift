913151bed33c717e23291a32a6023cca037ad259
etprogrammer <extraterrestrialprogrammer@gmail.com>
2023-02-06 07:58:05 +1100

    Initial commit

---
+++ ConfidentialTechnology/ConfidentialTechnologyApp.swift
@@ -0,0 +1,16 @@
+//
+//  ConfidentialTechnologyApp.swift
+//  ConfidentialTechnology
+//
+//
+
+import SwiftUI
+
+@main
+struct ConfidentialTechnologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfidentialTechnology/SecretSatellite.swift
@@ -0,0 +1,13 @@
+//
+//  SecretSatellite.swift
+//  ConfidentialTechnology
+//
+//
+
+import Foundation
+
+public struct SecretSatellite {
+    public static func sendSecretMessage() {
+        print("Secrets are secrets for a reason")
+    }
+}
